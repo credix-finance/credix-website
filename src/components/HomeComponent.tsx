bdc95ea33b3d6be08e35414b3b8c25225f189056
Maxim Piessen <info@maximpiessen.com>
2021-11-29 19:33:54 +0100

    ci cd works

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -9,7 +9,7 @@ export const HomeComponent = () => {
       <SvgIcon className="home-component--background-shape" icon={SVG_ICONS.HOME_BACKGROUND_SHAPE} color={SVG_COLORS.BLACK}/>
       <div className="h-flex-row cta-block">
         <div className="h-flex-column">
-          <span className="cta-block--title">qsdfqsdfsdCredit investing<br/> democratized</span>
+          <span className="cta-block--title">Credit investing<br/> democratized</span>
           <span className="cta-block--text">Be part of the new decentralized credit marketplace connecting investors with FinTechs in emerging markets.</span>
           <CredixButton className="cta-block--button">Join Discord</CredixButton>
         </div>
