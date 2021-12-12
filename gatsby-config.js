5ef6e7cd3ee51fd9dea0c039972456885f33efc7
mathijscop <mathijs.cop@rombit.be>
2021-12-12 19:12:52 +0100

    use Preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-preact",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
