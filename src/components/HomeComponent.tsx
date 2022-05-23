2a44e111471e9ceaad1c1ab9faaf2e82d12329f1
mathijscop <mathijs.cop@rombit.be>
2022-05-23 21:52:19 +0200

    added video to homepage

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,26 +1,44 @@
 import * as React from 'react'
 import { CredixButton } from './shared/buttons/CredixButton';
-import SvgIcon, { SVG_COLORS, SVG_ICONS } from './shared/svg-icon/SvgIcon';
-import { StaticImage } from 'gatsby-plugin-image';
 import { IWidthProps } from '../pages';
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import DogVideo from '../assets/video/Credix_WebsiteGradientA_02.mp4'
 
 export const HomeComponent = ({ isMobile }: IWidthProps) => {
 
+  const homeText = () => (
+    <div className="h-flex-column">
+      <h1 className="cta-block--title">Credit investing<br/> democratized</h1>
+      <span className="cta-block--text">Be part of the new decentralized credit marketplace connecting investors with FinTechs in emerging markets.</span>
+      <CredixButton className="cta-block--button" url={'https://discord.gg/5Ab8b8ueGs'}>Join Discord</CredixButton>
+    </div>
+  )
+
+  const gradientVideo = () => (
+    <div className="h-flex-column home-component--video">
+      <video controls={false} autoPlay muted loop>
+        <source src={DogVideo} type="video/mp4"/>
+      </video>
+    </div>
+  )
+
   return (
     <div className="h-flex-column home-component">
-      {
-        isMobile ?
-          <StaticImage className="home-component--background-shape--mobile" src="../assets/shapes/home-background-shape.png" alt="background"/>
-          :
-          <SvgIcon className="home-component--background-shape" icon={SVG_ICONS.HOME_BACKGROUND_SHAPE} color={SVG_COLORS.BLACK}/>
-      }
       <div className="h-flex-row cta-block">
-        <div className="h-flex-column">
-          <h1 className="cta-block--title">Credit investing<br/> democratized</h1>
-          <span className="cta-block--text">Be part of the new decentralized credit marketplace connecting investors with FinTechs in emerging markets.</span>
-          <CredixButton className="cta-block--button" url={'https://discord.gg/5Ab8b8ueGs'}>Join Discord</CredixButton>
-        </div>
+        { isMobile ?
+          <>
+            {gradientVideo()}
+            {homeText()}
+          </>
+          :
+          <>
+            {homeText()}
+            {gradientVideo()}
+          </>
+        }
+
+
       </div>
       <div className="h-flex-row home-component--arrow-row" id="journey">
         <a href="#journey" className="home-component--arrow"/>
