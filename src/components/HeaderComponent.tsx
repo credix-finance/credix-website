44a1050e37a682fa222fe77c5563c9d65f6c80e6
Jonas <boone.jonas@outlook.be>
2022-05-24 20:47:33 +0200

    refactor: highlight active page

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -4,16 +4,38 @@ import { CredixButton } from './shared/buttons/CredixButton';
 import { Drawer } from '@mui/material';
 import { IWidthProps } from '../hooks/useMobileWidth';
 import { openInNewTab } from '../utils/openInNewTab';
+import { Link } from 'gatsby';
+
+enum Page {
+    BORROWERS = 'borrowers',
+    INVESTORS = 'investors',
+}
+
+const getActiveLink = () => {
+  if (window.location.pathname.includes(Page.BORROWERS)) {
+    return Page.BORROWERS;
+  }
+  if (window.location.pathname.includes(Page.INVESTORS)) {
+    return Page.INVESTORS;
+  }
+}
 
 export const HeaderComponent = ({ isMobile }: IWidthProps) => {
-  const [isMenuOpen, setMenuOpen] = useState<boolean>();
+  const currentPage = getActiveLink();
+
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>();
 
   const headerLinks = () => {
-    // todo: fix urls
     return (
       <>
-        <span className="header-component--link" onClick={() => openInNewTab('/investors')}>Investors</span>
-        <span className="header-component--link" onClick={() => openInNewTab('/borrowers')}>Borrowers</span>
+        <div className="header-component--link">
+          <Link className="header-component--link--gatsby" to={'/investors'}>Investors</Link>
+          { currentPage === Page.INVESTORS && <div className="header-component--link--active"/>}
+        </div>
+        <div className="header-component--link">
+          <Link className="header-component--link--gatsby" to={'/borrowers'}>Borrowers</Link>
+          { currentPage === Page.BORROWERS && <div className="header-component--link--active"/>}
+        </div>
         <span className="header-component--link" onClick={() => openInNewTab('https://angel.co/company/credix')}>Team</span>
         <span className="header-component--link" onClick={() => openInNewTab('https://credix.gitbook.io/credix')}>Contact</span>
         <CredixButton className="header--links--button" url={'https://app.credix.finance/'}>Launch App</CredixButton>
@@ -28,7 +50,7 @@ export const HeaderComponent = ({ isMobile }: IWidthProps) => {
         <div className="h-flex-row h-flex-row--space-between h-flex-row--align-baseline header--links">
           {
             isMobile ?
-              <SvgIcon icon={SVG_ICONS.HAMBURGER_MENU} color={SVG_COLORS.WHITE} className="header-component--menu-button" onClick={() => setMenuOpen(!isMenuOpen)}/>
+              <SvgIcon icon={SVG_ICONS.HAMBURGER_MENU} color={SVG_COLORS.WHITE} className="header-component--menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}/>
               :
               headerLinks()
           }
@@ -36,11 +58,11 @@ export const HeaderComponent = ({ isMobile }: IWidthProps) => {
         <Drawer
           anchor={'right'}
           open={isMenuOpen}
-          onClose={() => setMenuOpen(false)}
+          onClose={() => setIsMenuOpen(false)}
           className="header-component--drawer"
         >
           <div className="h-flex-column header-component--drawer--links-column">
-            <SvgIcon icon={SVG_ICONS.CLOSE_MENU} color={SVG_COLORS.WHITE} className="header-component--menu-close-button" onClick={() => setMenuOpen(!isMenuOpen)}/>
+            <SvgIcon icon={SVG_ICONS.CLOSE_MENU} color={SVG_COLORS.WHITE} className="header-component--menu-close-button" onClick={() => setIsMenuOpen(!isMenuOpen)}/>
             {headerLinks()}
           </div>
         </Drawer>
