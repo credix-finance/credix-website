3efbf1bd6b0d419a6811d62b5d3c878e1130a87d
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2022-05-27 19:41:47 +0200

    fix document not supported error

--- src/utils/scrollfade.ts
+++ src/utils/scrollfade.ts
@@ -1,28 +1,30 @@
 // ScrollFade 0.1
 export const scrollFade = () => {
-  const fadeElements = document.getElementsByClassName('scrollFade');
-  // const viewportBottom = window.scrollY + window.innerHeight;
+  if (typeof document !== 'undefined') {
+    const fadeElements = document.getElementsByClassName('scrollFade');
+    // const viewportBottom = window.scrollY + window.innerHeight;
 
-  for (let index = 0; index < fadeElements.length; index++) {
-    const element = fadeElements[index];
-    const rect = element.getBoundingClientRect();
+    for (let index = 0; index < fadeElements.length; index++) {
+      const element = fadeElements[index];
+      const rect = element.getBoundingClientRect();
 
-    const elementFourth = rect.height/4;
-    const fadeInPoint = window.innerHeight - elementFourth;
-    const fadeOutPoint = -(rect.height/2);
+      const elementFourth = rect.height / 4;
+      const fadeInPoint = window.innerHeight - elementFourth;
+      const fadeOutPoint = -(rect.height / 2);
 
-    if (rect.top <= fadeInPoint/2) {
-      element.classList.add('scrollFade--visible');
-      element.classList.add('scrollFade--animate');
-      element.classList.remove('scrollFade--hidden');
-    } else {
-      element.classList.remove('scrollFade--visible');
-      element.classList.add('scrollFade--hidden');
-    }
+      if (rect.top <= fadeInPoint / 2) {
+        element.classList.add('scrollFade--visible');
+        element.classList.add('scrollFade--animate');
+        element.classList.remove('scrollFade--hidden');
+      } else {
+        element.classList.remove('scrollFade--visible');
+        element.classList.add('scrollFade--hidden');
+      }
 
-    if (rect.top <= fadeOutPoint/2) {
-      element.classList.remove('scrollFade--visible');
-      element.classList.add('scrollFade--hidden');
+      if (rect.top <= fadeOutPoint / 2) {
+        element.classList.remove('scrollFade--visible');
+        element.classList.add('scrollFade--hidden');
+      }
     }
   }
 }
