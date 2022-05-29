59e9264c33e12c2768ce622f5a9c71798f1c0d07
Jonas <boone.jonas@outlook.be>
2022-05-29 14:56:45 +0200

    feat: cookie banner

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-preact",
+    "gatsby-plugin-gdpr-cookies",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
@@ -61,6 +62,34 @@ module.exports = {
         // Defaults to false meaning LinkedIn Insight will only be loaded in production.
         includeInDevelopment: false
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: [
+            "G-2WM77E6YNH",
+            "AW-10862509247"
+          ],
+          cookieName: 'gatsby-gdpr-google-analytics',
+          anonymize: true,
+          allowAdFeatures: false
+        },
+        googleTagManager: {
+          trackingId: 'GTM-PJ3PP5W',
+          cookieName: 'gatsby-gdpr-google-tagmanager',
+          dataLayerName: 'dataLayer',
+        },
+        facebookPixel: {
+          pixelId: "1182663155872985",
+          cookieName: 'gatsby-gdpr-facebook-pixel',
+        },
+        linkedin: {
+          trackingId: '4229041',
+          cookieName: 'gatsby-gdpr-linked-in'
+        },
+        environments: ['production', 'development']
+      },
+    },
   ],
 };
---
+++ src/components/shared/cookies/CookieBanner.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useLocation } from '@reach/router'
+import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
+import { CookieButton } from '../buttons/CookieButton';
+
+export const CookieBanner = () => {
+  const [show, setShow] = useState(true);
+
+  const location = useLocation()
+
+  return (
+    <>
+      {
+        show && (
+          <div className="h-flex-row h-flex-row--align-center cookie-banner-component">
+            <div className="cookie-banner-component--text">We use third party cookies in order to optimize your
+                          experience.&nbsp;
+            <span className="cookie-banner-component--text cookie-banner-component--text--link h-cursor-pointer">View Cookie Policy</span>
+            </div>
+            <div className="h-flex-row h-flex-row--justify-center">
+              <CookieButton className="cookie-banner-component--button"
+                onClick={() => initializeAndTrack(location)}> Accept all</CookieButton>
+              <CookieButton className="cookie-banner-component--button--decline" onClick={() => setShow(false)}>Decline</CookieButton>
+            </div>
+          </div>
+        )
+      }
+    </>
+  );
+}
