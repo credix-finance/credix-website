d0400e1108bc6effab97b5d836475b5c546fbf44
Jonas <boone.jonas@outlook.be>
2022-06-01 21:37:22 +0200

    refactor: cleanup config gatsby and merge development

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,49 +21,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-2WM77E6YNH",
-          "AW-10862509247"
-        ],
-      },
-    },
-    {
-    resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: "1182663155872985",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-PJ3PP5W",
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" }
-      },
-    },
-    {
-      resolve: `gatsby-plugin-linkedin-insight`,
-      options: {
-        partnerId: `4229041`,
-
-        // Include LinkedIn Insight in development.
-        // Defaults to false meaning LinkedIn Insight will only be loaded in production.
-        includeInDevelopment: false
-      }
-    },
-    {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
