22ffda5e86fbdfe2f724d60dd98d09bc7fe23a77
Jonas <boone.jonas@outlook.be>
2022-06-01 21:38:27 +0200

    refactor: remove local state for cookie banner

--- src/components/shared/cookies/CookieBanner.tsx
+++ src/components/shared/cookies/CookieBanner.tsx
@@ -1,31 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from '@reach/router'
 import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
 import { CookieButton } from '../buttons/CookieButton';
 import { openInNewTab } from '../../../utils/openInNewTab';
 
 export const CookieBanner = () => {
-  const [show, setShow] = useState(true);
 
   const location = useLocation()
 
   return (
-    <>
-      {
-        show && (
-          <div className="h-flex-row h-flex-row--align-center cookie-banner-component">
-            <div className="cookie-banner-component--text">We use third party cookies in order to optimize your
+    <div className="h-flex-row h-flex-row--align-center cookie-banner-component">
+      <div className="cookie-banner-component--text">We use third party cookies in order to optimize your
                           experience.&nbsp;
-            <span className="cookie-banner-component--text cookie-banner-component--text--link h-cursor-pointer" onClick={() => openInNewTab('https://docs.credix.finance/other-links-and-resources/cookie-policy')}>View Cookie Policy</span>
-            </div>
-            <div className="cookie-banner-component--button-container">
-              <CookieButton className="cookie-banner-component--button"
-                onClick={() => initializeAndTrack(location)}> Accept all</CookieButton>
-              <CookieButton className="cookie-banner-component--button--decline" onClick={() => setShow(false)}>Decline</CookieButton>
-            </div>
-          </div>
-        )
-      }
-    </>
-  );
+      <span className="cookie-banner-component--text cookie-banner-component--text--link h-cursor-pointer" onClick={() => openInNewTab('https://docs.credix.finance/other-links-and-resources/cookie-policy')}>View Cookie Policy</span>
+      </div>
+      <div className="cookie-banner-component--button-container">
+        <CookieButton className="cookie-banner-component--button"
+          onClick={() => initializeAndTrack(location)}> Accept all</CookieButton>
+        <CookieButton className="cookie-banner-component--button--decline">Decline</CookieButton>
+      </div>
+    </div>
+  )
 }
