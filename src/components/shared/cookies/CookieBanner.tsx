7848f7db1e54937de421878f47561ccd628d5690
Jonas <boone.jonas@outlook.be>
2022-06-01 20:44:29 +0200

    refactor: mobile design for cookie banner

--- src/components/shared/cookies/CookieBanner.tsx
+++ src/components/shared/cookies/CookieBanner.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from '@reach/router'
 import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
 import { CookieButton } from '../buttons/CookieButton';
+import { openInNewTab } from '../../../utils/openInNewTab';
 
 export const CookieBanner = () => {
   const [show, setShow] = useState(true);
@@ -15,9 +16,9 @@ export const CookieBanner = () => {
           <div className="h-flex-row h-flex-row--align-center cookie-banner-component">
             <div className="cookie-banner-component--text">We use third party cookies in order to optimize your
                           experience.&nbsp;
-            <span className="cookie-banner-component--text cookie-banner-component--text--link h-cursor-pointer">View Cookie Policy</span>
+            <span className="cookie-banner-component--text cookie-banner-component--text--link h-cursor-pointer" onClick={() => openInNewTab('https://docs.credix.finance/other-links-and-resources/cookie-policy')}>View Cookie Policy</span>
             </div>
-            <div className="h-flex-row h-flex-row--justify-center">
+            <div className="cookie-banner-component--button-container">
               <CookieButton className="cookie-banner-component--button"
                 onClick={() => initializeAndTrack(location)}> Accept all</CookieButton>
               <CookieButton className="cookie-banner-component--button--decline" onClick={() => setShow(false)}>Decline</CookieButton>
