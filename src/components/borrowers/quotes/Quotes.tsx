20cfac6ad44afc1e5cb8dfd7cb49d8c8366ff60f
mathijscop <mathijs.cop@rombit.be>
2022-06-07 23:22:12 +0200

    make carousel generic

--- src/components/borrowers/quotes/Quotes.tsx
+++ src/components/borrowers/quotes/Quotes.tsx
@@ -1,42 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { IComponentProps } from '../../../hooks/useComponentProps';
 
-enum Quote {
-    A55 = 'A55',
-    TECREDI = 'TECREDI',
-    OTHER = 'OTHER',
-}
-
 export const Quotes = ({ isTablet, isLightTheme }: IComponentProps) => {
-  const [quote, setQuote] = React.useState(Quote.A55);
+  const [index, setIndex] = useState<number>(0);
+  const amountOfScroll = 1146
+  const amountOfItems = 3
 
   useEffect(() => {
     const interval = setInterval(() => {
-      switch (quote) {
-      case Quote.A55:
-        setQuote(Quote.TECREDI);
-        break;
-      case Quote.TECREDI:
-        setQuote(Quote.A55);
-        break;
-      default:
-        setQuote(Quote.A55);
-      }
+      setIndex(index < amountOfItems - 1 ? index + 1 : 0)
     }, 5000)
 
     return () => clearInterval(interval)
-  }, [quote])
+  }, [index])
 
   return (
     <div className="h-flex-column quotes-component">
       <div className="h-flex-row">
         <span className="quotes-component--title">Borrowers</span>
       </div>
-      <div className={`quotes-component--quote-row ${!isTablet && quote === Quote.A55 && 'quotes-component--quote-row--first-visible' || !isTablet && quote === Quote.TECREDI && 'quotes-component--quote-row--second-visible'}`}>
+      <div
+        className={'quotes-component--quote-row'}
+        style={{ transform: `translateX(-${!isTablet && amountOfScroll * index}px)` }}
+      >
         {
-          (!isTablet || isTablet && quote === Quote.A55) && (
-            <div className="quotes-component--card h-cursor-pointer" onClick={() => setQuote(Quote.A55)}>
+          (!isTablet || isTablet && index === 0) && (
+            <div className="quotes-component--card h-cursor-pointer" onClick={() => setIndex(0)}>
               <div className="quotes-component--text">
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                             "It is exciting to use the new capital markets, leveraged by decentralized finance and blockchain technologies, to help the new economy entrepreneurs. A55 is impressed by Credit's vision and the
@@ -57,8 +47,28 @@ export const Quotes = ({ isTablet, isLightTheme }: IComponentProps) => {
           )
         }
         {
-          (!isTablet || isTablet && quote === Quote.TECREDI) && (
-            <div className="quotes-component--card h-cursor-pointer" onClick={() => setQuote(Quote.TECREDI)}>
+          (!isTablet || isTablet && index === 1) && (
+            <div className="quotes-component--card h-cursor-pointer" onClick={() => setIndex(1)}>
+              <div className="quotes-component--text">
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                          "Credix is a great partner of Tecredi. Their solutions are rewriting the debt venture capital market and creating new possibilities for the whole ecosystem."
+              </div>
+              <div className="quotes-component--text--cited-person">
+                          Gustavo Pulga - CO CEO TECREDI
+              </div>
+              <span className="h-cursor-pointer">
+                {
+                  isLightTheme ? (<StaticImage className="quotes-component--background-shape quotes-component--background-shape--tecredi" src="../../../assets/logos/tecredi-dark.png" alt="tecredi logo"/>
+                  ) : (<StaticImage className="quotes-component--background-shape quotes-component--background-shape--tecredi" src="../../../assets/logos/tecredi-logo.png" alt="tecredi logo"/>
+                  )
+                }
+              </span>
+            </div>
+          )
+        }
+        {
+          (!isTablet || isTablet && index === 2) && (
+            <div className="quotes-component--card h-cursor-pointer" onClick={() => setIndex(2)}>
               <div className="quotes-component--text">
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                           "Credix is a great partner of Tecredi. Their solutions are rewriting the debt venture capital market and creating new possibilities for the whole ecosystem."
@@ -79,8 +89,9 @@ export const Quotes = ({ isTablet, isLightTheme }: IComponentProps) => {
       </div>
       <>
         <div className="h-flex-row h-flex-row--justify-center h-margin-top--triple">
-          <hr className="quotes-component--logo-row-mobile--divider h-cursor-pointer" style={{ opacity: quote === Quote.A55 ? 1 : 0.5 }} onClick={() => setQuote(Quote.A55)}/>
-          <hr className="quotes-component--logo-row-mobile--divider h-cursor-pointer" style={{ opacity: quote === Quote.TECREDI ? 1 : 0.5 }} onClick={() => setQuote(Quote.TECREDI)} />
+          <hr className="quotes-component--logo-row-mobile--divider h-cursor-pointer" style={{ opacity: index === 0 ? 1 : 0.5 }} onClick={() => setIndex(0)}/>
+          <hr className="quotes-component--logo-row-mobile--divider h-cursor-pointer" style={{ opacity: index === 1 ? 1 : 0.5 }} onClick={() => setIndex(1)}/>
+          <hr className="quotes-component--logo-row-mobile--divider h-cursor-pointer" style={{ opacity: index === 2 ? 1 : 0.5 }} onClick={() => setIndex(2)}/>
         </div>
       </>
     </div>
