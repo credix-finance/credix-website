d7e8888acf5735f2432d66cb05021ba62e32a156
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2022-08-10 14:08:55 +0200

    add docs and change title

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import SvgIcon, { SVG_COLORS, SVG_ICONS } from './shared/svg-icon/SvgIcon';
-import { CredixButton } from './shared/buttons/CredixButton';
-import { IComponentProps } from '../hooks/useComponentProps';
-import { Link } from 'gatsby';
-import { Drawer } from '@mui/material';
-import { LightThemeButton } from './shared/buttons/LightThemeButton';
+import React, { useEffect, useState } from "react";
+import SvgIcon, { SVG_COLORS, SVG_ICONS } from "./shared/svg-icon/SvgIcon";
+import { CredixButton } from "./shared/buttons/CredixButton";
+import { IComponentProps } from "../hooks/useComponentProps";
+import { Link } from "gatsby";
+import { Drawer } from "@mui/material";
+import { LightThemeButton } from "./shared/buttons/LightThemeButton";
 
 enum EPage {
-    HOME = 'home',
-    BORROWERS = 'borrowers',
-    INVESTORS = 'investors',
+  HOME = "home",
+  BORROWERS = "borrowers",
+  INVESTORS = "investors",
 }
 
 const getActiveLink = (pathname?: string) => {
@@ -19,15 +19,21 @@ const getActiveLink = (pathname?: string) => {
   if (pathname && pathname.includes(EPage.INVESTORS)) {
     return EPage.INVESTORS;
   } else {
-    return EPage.HOME
+    return EPage.HOME;
   }
-}
+};
 
-export const HeaderComponent = ({ isMobile, isLightTheme, setLightTheme, ...locationProps }: IComponentProps) => {
-
+export const HeaderComponent = ({
+  isMobile,
+  isLightTheme,
+  setLightTheme,
+  ...locationProps
+}: IComponentProps) => {
   const { location } = locationProps;
 
-  const [currentPage, setCurrentPage] = useState<string | undefined>(getActiveLink(location?.pathname));
+  const [currentPage, setCurrentPage] = useState<string | undefined>(
+    getActiveLink(location?.pathname)
+  );
 
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>();
 
@@ -36,63 +42,123 @@ export const HeaderComponent = ({ isMobile, isLightTheme, setLightTheme, ...loca
   }, [location]);
 
   const mail = (e: any) => {
-    window.location = 'mailto:info@credix.finance' as unknown as Location;
-    e.preventDefault()
-  }
+    window.location = "mailto:info@credix.finance" as unknown as Location;
+    e.preventDefault();
+  };
 
   const headerLinks = () => {
     return (
       <>
-        <div className="header-component--link" style={{ marginTop: !isMobile && currentPage === EPage.INVESTORS ? '12px' : 0 }}>
-          <Link className="header-component--link--gatsby" to={'/investors'}>Investors</Link>
-          { !isMobile && currentPage === EPage.INVESTORS && <div className="header-component--link--active"/>}
+        <div
+          className="header-component--link"
+          style={{
+            marginTop:
+              !isMobile && currentPage === EPage.INVESTORS ? "12px" : 0,
+          }}
+        >
+          <Link className="header-component--link--gatsby" to={"/investors"}>
+            Investors
+          </Link>
+          {!isMobile && currentPage === EPage.INVESTORS && (
+            <div className="header-component--link--active" />
+          )}
         </div>
-        <div className="header-component--link" style={{ marginTop: !isMobile && currentPage === EPage.BORROWERS ? '12px' : 0 }}>
-          <Link className="header-component--link--gatsby" to={'/borrowers'}>Borrowers</Link>
-          { !isMobile && currentPage === EPage.BORROWERS && <div className="header-component--link--active"/>}
+        <div
+          className="header-component--link"
+          style={{
+            marginTop:
+              !isMobile && currentPage === EPage.BORROWERS ? "12px" : 0,
+          }}
+        >
+          <Link className="header-component--link--gatsby" to={"/borrowers"}>
+            Borrowers
+          </Link>
+          {!isMobile && currentPage === EPage.BORROWERS && (
+            <div className="header-component--link--active" />
+          )}
         </div>
-        <span className="header-component--link" onClick={mail}>Contact</span>
-        <CredixButton className={`header--links--button ${isLightTheme ? 'h-color-white' : 'h-color-black'}`} url={'https://app.credix.finance/'}>Launch App</CredixButton>
-        {
-          !isMobile && <LightThemeButton isLightTheme={isLightTheme} onChange={(bool) => setLightTheme && setLightTheme(bool)}/>
-        }
-
+        <span className="header-component--link" onClick={mail}>
+          Docs
+        </span>
+        <CredixButton
+          className={`header--links--button ${
+            isLightTheme ? "h-color-white" : "h-color-black"
+          }`}
+          url={"https://docs.credix.finance/"}
+        >
+          Launch App
+        </CredixButton>
+        {!isMobile && (
+          <LightThemeButton
+            isLightTheme={isLightTheme}
+            onChange={(bool) => setLightTheme && setLightTheme(bool)}
+          />
+        )}
       </>
-    )
-  }
+    );
+  };
 
   return (
     <div className="h-flex-column header-component">
       <div className="h-flex-row h-flex-row--space-between header">
-        <SvgIcon icon={SVG_ICONS.CREDIX_LOGO} className="h-cursor-pointer" color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE} onClick={() => window.location = '..' as unknown as Location}/>
+        <SvgIcon
+          icon={SVG_ICONS.CREDIX_LOGO}
+          className="h-cursor-pointer"
+          color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE}
+          onClick={() => (window.location = ".." as unknown as Location)}
+        />
         <div className="h-flex-row h-flex-row--space-between h-flex-row--align-center header--links">
-          {
-            isMobile ?
-              <SvgIcon icon={SVG_ICONS.HAMBURGER_MENU} color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE} className="header-component--menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}/>
-              :
-              headerLinks()
-          }
+          {isMobile ? (
+            <SvgIcon
+              icon={SVG_ICONS.HAMBURGER_MENU}
+              color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE}
+              className="header-component--menu-button"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
+          ) : (
+            headerLinks()
+          )}
         </div>
         <Drawer
-          anchor={'right'}
+          anchor={"right"}
           open={isMenuOpen}
           onClose={() => setIsMenuOpen(false)}
-          className={`header-component--drawer ${isLightTheme ? 'h-background-color-white' : 'h-background-color-black'}`}
+          className={`header-component--drawer ${
+            isLightTheme
+              ? "h-background-color-white"
+              : "h-background-color-black"
+          }`}
         >
           <div className="h-flex-row h-flex-column--align-end h-flex-row--space-between header-component--icons">
-            <SvgIcon icon={SVG_ICONS.CREDIX_LOGO_MOBILE} color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE} className="header-component--logo" onClick={() => window.location = '..' as unknown as Location}/>
+            <SvgIcon
+              icon={SVG_ICONS.CREDIX_LOGO_MOBILE}
+              color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE}
+              className="header-component--logo"
+              onClick={() => (window.location = ".." as unknown as Location)}
+            />
             <div className="h-flex-row h-flex-row--align-baseline">
-              {
-                isMobile && <LightThemeButton isLightTheme={isLightTheme} onChange={(bool) => setLightTheme && setLightTheme(bool)}/>
-              }
-              <SvgIcon icon={SVG_ICONS.CLOSE_MENU} color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE} className="header-component--menu-close-button" onClick={() => setIsMenuOpen(!isMenuOpen)}/>
+              {isMobile && (
+                <LightThemeButton
+                  isLightTheme={isLightTheme}
+                  onChange={(bool) => setLightTheme && setLightTheme(bool)}
+                />
+              )}
+              <SvgIcon
+                icon={SVG_ICONS.CLOSE_MENU}
+                color={isLightTheme ? SVG_COLORS.BLACK : SVG_COLORS.WHITE}
+                className="header-component--menu-close-button"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              />
             </div>
           </div>
-          <div className="h-flex-column header-component--drawer--links-column" style={{ color: isLightTheme ? 'black' : 'white' }}>
+          <div
+            className="h-flex-column header-component--drawer--links-column"
+            style={{ color: isLightTheme ? "black" : "white" }}
+          >
             {headerLinks()}
           </div>
         </Drawer>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import '../theme/index.scss';
-import { HomeComponent } from '../components/HomeComponent';
-import { HeaderComponent } from '../components/HeaderComponent';
-import { CTATeamComponent } from '../components/CTATeamComponent';
-import { CoreTeamComponent } from '../components/CoreTeamComponent';
-import { FooterComponent } from '../components/FooterComponent';
-import Helmet from 'react-helmet';
-import fav from '../../static/favicon.png';
-import preview from '../../static/preview_large.png';
-import { ELocalStorage, useComponentProps } from '../hooks/useComponentProps';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { ParallaxComponent } from '../components/ParallaxComponent';
-import { BackersBorrowersComponent } from '../components/BackersBorrowersComponent';
-import { MarqueeComponent } from '../components/shared/marquee/MarqueeComponent';
-import { CTAHomeComponent } from '../components/CTAHomeComponent';
-import { useEffect, useState } from 'react';
-import { CookieBanner } from '../components/shared/cookies/CookieBanner';
+import * as React from "react";
+import "../theme/index.scss";
+import { HomeComponent } from "../components/HomeComponent";
+import { HeaderComponent } from "../components/HeaderComponent";
+import { CTATeamComponent } from "../components/CTATeamComponent";
+import { CoreTeamComponent } from "../components/CoreTeamComponent";
+import { FooterComponent } from "../components/FooterComponent";
+import Helmet from "react-helmet";
+import fav from "../../static/favicon.png";
+import preview from "../../static/preview_large.png";
+import { ELocalStorage, useComponentProps } from "../hooks/useComponentProps";
+import { ParallaxProvider } from "react-scroll-parallax";
+import { ParallaxComponent } from "../components/ParallaxComponent";
+import { BackersBorrowersComponent } from "../components/BackersBorrowersComponent";
+import { MarqueeComponent } from "../components/shared/marquee/MarqueeComponent";
+import { CTAHomeComponent } from "../components/CTAHomeComponent";
+import { useEffect, useState } from "react";
+import { CookieBanner } from "../components/shared/cookies/CookieBanner";
 
 const IndexPage = () => {
   const { width, mobileWidth, getLightTheme } = useComponentProps();
@@ -34,108 +34,106 @@ const IndexPage = () => {
   }, []);
 
   return (
-    <main className={`${isLightTheme ? 'light' : 'dark'}`}>
+    <main className={`${isLightTheme ? "light" : "dark"}`}>
       <Helmet
-        titleTemplate="%s - Credit investing democratized"
-        link={[{ rel: 'shortcut icon', type: 'image/png', href: `${fav}` }]}
+        titleTemplate="%s - Credit investing made global"
+        link={[{ rel: "shortcut icon", type: "image/png", href: `${fav}` }]}
         meta={[
           {
-            name: 'description',
+            name: "description",
             content:
-              'CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.'
+              "CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.",
           },
           {
-            name: 'keywords',
+            name: "keywords",
             content:
-              'fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets'
+              "fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets",
           },
           {
-            name: 'robots',
-            content: 'index, nofollow'
+            name: "robots",
+            content: "index, nofollow",
           },
           {
-            name: 'title',
-            content: 'Credix'
+            name: "title",
+            content: "Credix",
           },
           {
-            property: 'og:image',
-            content: preview
+            property: "og:image",
+            content: preview,
           },
           {
-            property: 'og:image:alt',
-            content: 'Credix - Credit investing made global'
+            property: "og:image:alt",
+            content: "Credix - Credit investing made global",
           },
           {
-            property: 'og:title',
-            content: 'Credix - Credit investing made global'
+            property: "og:title",
+            content: "Credix - Credit investing made global",
           },
           {
-            property: 'og:description',
+            property: "og:description",
             content:
-              'CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.'
+              "CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.",
           },
           {
-            property: 'og:url',
-            content: 'https://credix.finance'
+            property: "og:url",
+            content: "https://credix.finance",
           },
           {
-            property: 'og:type',
-            content: 'website'
+            property: "og:type",
+            content: "website",
           },
           {
-            property: 'twitter:image',
-            content: preview
+            property: "twitter:image",
+            content: preview,
           },
           {
-            property: 'twitter:description',
+            property: "twitter:description",
             content:
-              'CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.'
+              "CREDIX is a decentralized credit marketplace connecting investors with FinTechs in emerging markets. Learn more about the future of debt financing now.",
           },
           {
-            property: 'twitter:title',
-            content: 'Credix - Credit investing made global'
+            property: "twitter:title",
+            content: "Credix - Credit investing made global",
           },
           {
-            name: 'twitter:card',
-            content: 'summary_large_image'
+            name: "twitter:card",
+            content: "summary_large_image",
           },
           {
-            name: 'theme-color',
-            content: '#1F1F1F'
-          }
+            name: "theme-color",
+            content: "#1F1F1F",
+          },
         ]}
       >
         <title lang="en">Credix</title>
-        <html lang="en"/>
+        <html lang="en" />
       </Helmet>
-      {
-        typeof isLightTheme !== 'undefined' &&
-          <>
-            <MarqueeComponent/>
-            <CookieBanner/>
-            <ParallaxProvider>
-              <HeaderComponent
-                isMobile={!!width && width < mobileWidth}
-                isLightTheme={isLightTheme}
-                setLightTheme={setLightTheme}
-              />
-              <HomeComponent isMobile={!!width && width < mobileWidth}/>
-              <ParallaxComponent
-                isMobile={!!width && width < mobileWidth}
-                isLightTheme={isLightTheme}
-              />
-              <BackersBorrowersComponent/>
-              <CoreTeamComponent isLightTheme={isLightTheme}/>
-              <CTATeamComponent/>
-              <CTAHomeComponent/>
-              <FooterComponent
-                isMobile={!!width && width < mobileWidth}
-                isLightTheme={isLightTheme}
-              />
-            </ParallaxProvider>
-          </>
-      }
-
+      {typeof isLightTheme !== "undefined" && (
+        <>
+          <MarqueeComponent />
+          <CookieBanner />
+          <ParallaxProvider>
+            <HeaderComponent
+              isMobile={!!width && width < mobileWidth}
+              isLightTheme={isLightTheme}
+              setLightTheme={setLightTheme}
+            />
+            <HomeComponent isMobile={!!width && width < mobileWidth} />
+            <ParallaxComponent
+              isMobile={!!width && width < mobileWidth}
+              isLightTheme={isLightTheme}
+            />
+            <BackersBorrowersComponent />
+            <CoreTeamComponent isLightTheme={isLightTheme} />
+            <CTATeamComponent />
+            <CTAHomeComponent />
+            <FooterComponent
+              isMobile={!!width && width < mobileWidth}
+              isLightTheme={isLightTheme}
+            />
+          </ParallaxProvider>
+        </>
+      )}
     </main>
   );
 };
