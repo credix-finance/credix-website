dc48d608e31c9564ce80fb0a10127f0a77035b1a
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2022-08-10 14:45:16 +0200

    ...

--- src/components/shared/marquee/MarqueeComponent.tsx
+++ src/components/shared/marquee/MarqueeComponent.tsx
@@ -1,42 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import Marquee from 'react-fast-marquee';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Marquee from "react-fast-marquee";
+import axios from "axios";
 
 export const MarqueeComponent = () => {
-  const [tvl, setTvl] = useState('');
-  const [creditOutstanding, setCreditOutstanding] = useState('');
+  const [tvl, setTvl] = useState("");
+  const [creditOutstanding, setCreditOutstanding] = useState("");
+  const [financingFee, setFinancingFee] = useState("");
 
   useEffect(() => {
-    const fetchData = async() => {
-      const result = await axios.get('https://api.credix.finance/stats/markets/credix-marketplace');
+    const fetchData = async () => {
+      const result = await axios.get(
+        "https://credix-market-stats.credix.workers.dev/?cached=True"
+      );
       const { data } = result;
-
-      const formattedTvl = Intl.NumberFormat('en', {
-        notation: 'compact',
+      console.log("dataaaaa", data);
+      const formattedTvl = Intl.NumberFormat("en", {
+        notation: "compact",
         minimumFractionDigits: 0,
-        maximumFractionDigits: 1
-      }).format(data.liquidity_pool_amount.uiAmount + data.total_outstanding_credit.uiAmount);
-      const formattedCreditOutstanding = Intl.NumberFormat('en', {
-        notation: 'compact',
+        maximumFractionDigits: 1,
+      }).format(data.TVL.uiAmount);
+      const formattedCreditOutstanding = Intl.NumberFormat("en", {
+        notation: "compact",
         minimumFractionDigits: 0,
-        maximumFractionDigits: 1
+        maximumFractionDigits: 1,
       }).format(data.total_outstanding_credit.uiAmount);
+      const formattedFinancingFee = Intl.NumberFormat("en", {
+        notation: "compact",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+      }).format(data.weighted_average_financing_fee);
 
       setTvl(formattedTvl);
       setCreditOutstanding(formattedCreditOutstanding);
-    }
+      setFinancingFee(formattedFinancingFee);
+    };
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <Marquee loop={0} speed={50} gradient={false}>
-      <span style={{ marginLeft: '64px' }}>TVL:<b> {tvl ? `${tvl} USDC` : '...'}</b></span>
-      <span style={{ marginLeft: '64px' }}>Credit Outstanding:<b> {creditOutstanding ? `${creditOutstanding} USDC` : '...'}</b></span>
-      <span style={{ marginLeft: '64px' }}>Liquidity Pool APY:<b> 13,5%</b></span>
-      <span style={{ marginLeft: '64px' }}>TVL:<b> {tvl ? `${tvl} USDC` : '...'}</b></span>
-      <span style={{ marginLeft: '64px' }}>Credit Outstanding:<b> {creditOutstanding ? `${creditOutstanding} USDC` : '...'}</b></span>
-      <span style={{ marginLeft: '64px' }}>Liquidity Pool APY:<b> 13,5%</b></span>
+      <span style={{ marginLeft: "64px" }}>
+        TVL:<b> {tvl ? `${tvl} USDC` : "..."}</b>
+      </span>
+      <span style={{ marginLeft: "64px" }}>
+        Credit Outstanding:
+        <b> {creditOutstanding ? `${creditOutstanding} USDC` : "..."}</b>
+      </span>
+      <span style={{ marginLeft: "64px" }}>
+        Average financing fee:
+        <b> {financingFee ? `${financingFee * 100} %` : "..."}</b>
+      </span>
+      <span style={{ marginLeft: "64px" }}>
+        TVL:<b> {tvl ? `${tvl} USDC` : "..."}</b>
+      </span>
+      <span style={{ marginLeft: "64px" }}>
+        Credit Outstanding:
+        <b> {creditOutstanding ? `${creditOutstanding} USDC` : "..."}</b>
+      </span>
+      <span style={{ marginLeft: "64px" }}>
+        Average financing fee:
+        <b> {financingFee ? `${financingFee * 100} %` : "..."}</b>
+      </span>
     </Marquee>
-
   );
-}
+};
