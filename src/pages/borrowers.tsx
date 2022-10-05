ab50327246c7212036acf63a038422179233531b
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2022-10-05 18:53:23 +0200

    temp disable stats

--- src/pages/borrowers.tsx
+++ src/pages/borrowers.tsx
@@ -14,131 +14,131 @@ import { MarqueeComponent } from "../components/shared/marquee/MarqueeComponent"
 import { CookieBanner } from "../components/shared/cookies/CookieBanner";
 
 const BorrowersPage = () => {
-  const { width, mobileWidth, tabletWidth, getLightTheme } =
-    useComponentProps();
+	const { width, mobileWidth, tabletWidth, getLightTheme } =
+		useComponentProps();
 
-  const [isLightTheme, setIsLightTheme] = useState<boolean | undefined>(
-    undefined
-  );
+	const [isLightTheme, setIsLightTheme] = useState<boolean | undefined>(
+		undefined
+	);
 
-  const setLightTheme = (bool: boolean) => {
-    setIsLightTheme(bool);
-    localStorage.setItem(ELocalStorage.LIGHT_THEME, JSON.stringify(bool));
-  };
+	const setLightTheme = (bool: boolean) => {
+		setIsLightTheme(bool);
+		localStorage.setItem(ELocalStorage.LIGHT_THEME, JSON.stringify(bool));
+	};
 
-  useEffect(() => {
-    setIsLightTheme(getLightTheme());
-  }, []);
+	useEffect(() => {
+		setIsLightTheme(getLightTheme());
+	}, []);
 
-  return (
-    <main className={`${isLightTheme ? "light" : "dark"}`}>
-      <Helmet
-        titleTemplate="%s - Credit investing made global"
-        link={[{ rel: "shortcut icon", type: "image/png", href: `${fav}` }]}
-        meta={[
-          {
-            name: "description",
-            content:
-              "Secure debt capital from international investors is now easier than before.",
-          },
-          {
-            name: "keywords",
-            content:
-              "fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets",
-          },
-          {
-            name: "robots",
-            content: "index, nofollow",
-          },
-          {
-            name: "title",
-            content: "Credix",
-          },
-          {
-            property: "og:image",
-            content: preview,
-          },
-          {
-            property: "og:image:alt",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            property: "og:title",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            property: "og:description",
-            content:
-              "Secure debt capital from international investors is now easier than before.",
-          },
-          {
-            property: "og:url",
-            content: "https://credix.finance",
-          },
-          {
-            property: "og:type",
-            content: "website",
-          },
-          {
-            property: "twitter:image",
-            content: preview,
-          },
-          {
-            property: "twitter:description",
-            content:
-              "Secure debt capital from international investors is now easier than before.",
-          },
-          {
-            property: "twitter:title",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            name: "twitter:card",
-            content: "summary_large_image",
-          },
-          {
-            name: "theme-color",
-            content: "#1F1F1F",
-          },
-        ]}
-      >
-        <title lang="en">Credix</title>
-        <html lang="en" />
-      </Helmet>
-      {typeof isLightTheme !== "undefined" && (
-        <>
-          <CookieBanner />
-          <MarqueeComponent />
-          <Location>
-            {(locationProps) => (
-              <HeaderComponent
-                {...locationProps}
-                isMobile={!!width && width < mobileWidth}
-                isLightTheme={isLightTheme}
-                setLightTheme={setLightTheme}
-              />
-            )}
-          </Location>
-          <BorrowersHomeComponent
-            isMobile={!!width && width < mobileWidth}
-            isTablet={!!width && width < tabletWidth}
-          />
-          <BorrowersCTAComponent />
-          <Quotes
-            isTablet={!!width && width < tabletWidth}
-            isLightTheme={isLightTheme}
-          />
-          <BorrowersCTARegisterComponent
-            isMobile={!!width && width < mobileWidth}
-          />
-          <FooterComponent
-            isMobile={!!width && width < mobileWidth}
-            isLightTheme={isLightTheme}
-          />
-        </>
-      )}
-    </main>
-  );
+	return (
+		<main className={`${isLightTheme ? "light" : "dark"}`}>
+			<Helmet
+				titleTemplate="%s - Credit investing made global"
+				link={[{ rel: "shortcut icon", type: "image/png", href: `${fav}` }]}
+				meta={[
+					{
+						name: "description",
+						content:
+							"Secure debt capital from international investors is now easier than before.",
+					},
+					{
+						name: "keywords",
+						content:
+							"fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets",
+					},
+					{
+						name: "robots",
+						content: "index, nofollow",
+					},
+					{
+						name: "title",
+						content: "Credix",
+					},
+					{
+						property: "og:image",
+						content: preview,
+					},
+					{
+						property: "og:image:alt",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						property: "og:title",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						property: "og:description",
+						content:
+							"Secure debt capital from international investors is now easier than before.",
+					},
+					{
+						property: "og:url",
+						content: "https://credix.finance",
+					},
+					{
+						property: "og:type",
+						content: "website",
+					},
+					{
+						property: "twitter:image",
+						content: preview,
+					},
+					{
+						property: "twitter:description",
+						content:
+							"Secure debt capital from international investors is now easier than before.",
+					},
+					{
+						property: "twitter:title",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						name: "twitter:card",
+						content: "summary_large_image",
+					},
+					{
+						name: "theme-color",
+						content: "#1F1F1F",
+					},
+				]}
+			>
+				<title lang="en">Credix</title>
+				<html lang="en" />
+			</Helmet>
+			{typeof isLightTheme !== "undefined" && (
+				<>
+					<CookieBanner />
+					{/* <MarqueeComponent /> */}
+					<Location>
+						{(locationProps) => (
+							<HeaderComponent
+								{...locationProps}
+								isMobile={!!width && width < mobileWidth}
+								isLightTheme={isLightTheme}
+								setLightTheme={setLightTheme}
+							/>
+						)}
+					</Location>
+					<BorrowersHomeComponent
+						isMobile={!!width && width < mobileWidth}
+						isTablet={!!width && width < tabletWidth}
+					/>
+					<BorrowersCTAComponent />
+					<Quotes
+						isTablet={!!width && width < tabletWidth}
+						isLightTheme={isLightTheme}
+					/>
+					<BorrowersCTARegisterComponent
+						isMobile={!!width && width < mobileWidth}
+					/>
+					<FooterComponent
+						isMobile={!!width && width < mobileWidth}
+						isLightTheme={isLightTheme}
+					/>
+				</>
+			)}
+		</main>
+	);
 };
 
 export default BorrowersPage;
--- src/pages/investors.tsx
+++ src/pages/investors.tsx
@@ -13,124 +13,124 @@ import { Location } from "@reach/router";
 import { CookieBanner } from "../components/shared/cookies/CookieBanner";
 
 const InvestorsPage = () => {
-  const { width, mobileWidth, getLightTheme } = useComponentProps();
+	const { width, mobileWidth, getLightTheme } = useComponentProps();
 
-  const [isLightTheme, setIsLightTheme] = useState<boolean | undefined>(
-    undefined
-  );
+	const [isLightTheme, setIsLightTheme] = useState<boolean | undefined>(
+		undefined
+	);
 
-  const setLightTheme = (bool: boolean) => {
-    setIsLightTheme(bool);
-    localStorage.setItem(ELocalStorage.LIGHT_THEME, JSON.stringify(bool));
-  };
+	const setLightTheme = (bool: boolean) => {
+		setIsLightTheme(bool);
+		localStorage.setItem(ELocalStorage.LIGHT_THEME, JSON.stringify(bool));
+	};
 
-  useEffect(() => {
-    setIsLightTheme(getLightTheme());
-  }, []);
+	useEffect(() => {
+		setIsLightTheme(getLightTheme());
+	}, []);
 
-  return (
-    <main className={`${isLightTheme ? "light" : "dark"}`}>
-      <Helmet
-        titleTemplate="%s - Credit investing made global"
-        link={[{ rel: "shortcut icon", type: "image/png", href: `${fav}` }]}
-        meta={[
-          {
-            name: "description",
-            content:
-              "Get access to exclusive, high-quality private credit investments.",
-          },
-          {
-            name: "keywords",
-            content:
-              "fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets",
-          },
-          {
-            name: "robots",
-            content: "index, nofollow",
-          },
-          {
-            name: "title",
-            content: "Credix",
-          },
-          {
-            property: "og:image",
-            content: preview,
-          },
-          {
-            property: "og:image:alt",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            property: "og:title",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            property: "og:description",
-            content:
-              "Get access to exclusive, high-quality private credit investments.",
-          },
-          {
-            property: "og:url",
-            content: "https://credix.finance",
-          },
-          {
-            property: "og:type",
-            content: "website",
-          },
-          {
-            property: "twitter:image",
-            content: preview,
-          },
-          {
-            property: "twitter:description",
-            content:
-              "Get access to exclusive, high-quality private credit investments.",
-          },
-          {
-            property: "twitter:title",
-            content: "Credix - Credit investing made global",
-          },
-          {
-            name: "twitter:card",
-            content: "summary_large_image",
-          },
-          {
-            name: "theme-color",
-            content: "#1F1F1F",
-          },
-        ]}
-      >
-        <title lang="en">Credix</title>
-        <html lang="en" />
-      </Helmet>
-      {typeof isLightTheme !== "undefined" && (
-        <>
-          <MarqueeComponent />
-          <CookieBanner />
-          <Location>
-            {(locationProps) => (
-              <HeaderComponent
-                {...locationProps}
-                isMobile={!!width && width < mobileWidth}
-                isLightTheme={isLightTheme}
-                setLightTheme={setLightTheme}
-              />
-            )}
-          </Location>
-          <InvestorsHomeComponent isMobile={!!width && width < mobileWidth} />
-          <InvestorsCTAComponent />
-          <InvestorsList
-            isMobile={!!width && width < mobileWidth}
-            isLightTheme={isLightTheme}
-          />
-          <FooterComponent
-            isMobile={!!width && width < mobileWidth}
-            isLightTheme={isLightTheme}
-          />
-        </>
-      )}
-    </main>
-  );
+	return (
+		<main className={`${isLightTheme ? "light" : "dark"}`}>
+			<Helmet
+				titleTemplate="%s - Credit investing made global"
+				link={[{ rel: "shortcut icon", type: "image/png", href: `${fav}` }]}
+				meta={[
+					{
+						name: "description",
+						content:
+							"Get access to exclusive, high-quality private credit investments.",
+					},
+					{
+						name: "keywords",
+						content:
+							"fintech, finance, capital, blockchain, investors, borrowers, assets, emerging countries, capital markets",
+					},
+					{
+						name: "robots",
+						content: "index, nofollow",
+					},
+					{
+						name: "title",
+						content: "Credix",
+					},
+					{
+						property: "og:image",
+						content: preview,
+					},
+					{
+						property: "og:image:alt",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						property: "og:title",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						property: "og:description",
+						content:
+							"Get access to exclusive, high-quality private credit investments.",
+					},
+					{
+						property: "og:url",
+						content: "https://credix.finance",
+					},
+					{
+						property: "og:type",
+						content: "website",
+					},
+					{
+						property: "twitter:image",
+						content: preview,
+					},
+					{
+						property: "twitter:description",
+						content:
+							"Get access to exclusive, high-quality private credit investments.",
+					},
+					{
+						property: "twitter:title",
+						content: "Credix - Credit investing made global",
+					},
+					{
+						name: "twitter:card",
+						content: "summary_large_image",
+					},
+					{
+						name: "theme-color",
+						content: "#1F1F1F",
+					},
+				]}
+			>
+				<title lang="en">Credix</title>
+				<html lang="en" />
+			</Helmet>
+			{typeof isLightTheme !== "undefined" && (
+				<>
+					{/* <MarqueeComponent /> */}
+					<CookieBanner />
+					<Location>
+						{(locationProps) => (
+							<HeaderComponent
+								{...locationProps}
+								isMobile={!!width && width < mobileWidth}
+								isLightTheme={isLightTheme}
+								setLightTheme={setLightTheme}
+							/>
+						)}
+					</Location>
+					<InvestorsHomeComponent isMobile={!!width && width < mobileWidth} />
+					<InvestorsCTAComponent />
+					<InvestorsList
+						isMobile={!!width && width < mobileWidth}
+						isLightTheme={isLightTheme}
+					/>
+					<FooterComponent
+						isMobile={!!width && width < mobileWidth}
+						isLightTheme={isLightTheme}
+					/>
+				</>
+			)}
+		</main>
+	);
 };
 
 export default InvestorsPage;
